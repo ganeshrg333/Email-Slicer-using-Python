019c4e2a953958bcb510f7cecdae7e221529081e
Ganesh Ravindra Gaonkar <55882821+ganeshrg333@users.noreply.github.com>
2021-02-13 12:59:30 +0530

    Update Email-Slicer-using-Python.py

--- Email-Slicer-using-Python.py
+++ Email-Slicer-using-Python.py
@@ -11,7 +11,10 @@ format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
 print(format_)
 
 
-# In[ ]:
+# The code above is very simple and easy to understand. 
+# We take user input and use the strip function at the same time to remove white space if any. 
+# Then we are finding the index of ‘@’ symbol of the user input. Then we store the index into a variable known as domain_name to split the email into two parts;
+# the user name and the domain.:
 
 
 
